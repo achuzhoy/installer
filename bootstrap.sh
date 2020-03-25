73c2f7c1def034b506b1024c9af84a5904d0bd20
Eran Cohen <eran@stratoscale.com>
2020-03-25 10:05:35 +0200

    Added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euoE pipefail ## -E option will cause functions to inherit trap
+
+OPENSHIFT_INSTALLER_IMAGE=docker.io/eranco/openshift-installer:ignition
+podman_run() {
+    echo "${@}"
+    podman run --net=host "${@}"
+}
+
+echo "Generating ignition configs"
+
+podman_run \
+        --volume "$PWD/install-dir:/install-dir:Z" \
+        --env OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=quay.io/openshift-release-dev/ocp-release-nightly@sha256:6e444e55eba18a03926a793607952c329af547867ed3739882f1de02c4f05fc3 \
+        "${OPENSHIFT_INSTALLER_IMAGE}" \
+        create ignition-configs \
+        --dir=/install-dir
+
+
+echo "Extracting machine-config-daemon"
+podman_run \
+        --volume "$PWD/install-dir:/install-dir:Z" \
+        --entrypoint cp \
+        "${OPENSHIFT_INSTALLER_IMAGE}" \
+        /machine-config-daemon /install-dir/
+
+echo "Writing bootstrap ignition to disk"
+./install-dir/machine-config-daemon start --node-name $(hostname) --root-mount / --once-from $(pwd)/install-dir/bootstrap.ign --skip-reboot
